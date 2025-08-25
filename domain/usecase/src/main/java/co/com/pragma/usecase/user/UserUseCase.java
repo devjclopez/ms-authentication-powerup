5640239070d930eacd5b09c584853c998deda2cb
Josimar Albert Castro Lopez <dev.jclopez@gmail.com>
2025-08-25 13:03:36 -0500

    feat: add R2DBC transaction management

--- domain/usecase/src/main/java/co/com/pragma/usecase/user/UserUseCase.java
+++ domain/usecase/src/main/java/co/com/pragma/usecase/user/UserUseCase.java
@@ -2,7 +2,6 @@ package co.com.pragma.usecase.user;
 
 import co.com.pragma.model.user.User;
 import co.com.pragma.model.user.UserValidator;
-import co.com.pragma.model.user.exceptions.InvalidUserException;
 import co.com.pragma.model.user.gateways.UserRepository;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -18,15 +17,6 @@ public class UserUseCase {
           UserValidator.validate(userToValidate);
           return userToValidate;
         })
-        .flatMap(validUser -> validateUniqueEmail(validUser.getEmail())
-            .then(Mono.just(validUser)))
         .flatMap(userRepository::register);
-  }
-
-  private Mono<Void> validateUniqueEmail(String email) {
-    return userRepository.existUserByEmail(email)
-        .flatMap(exists -> exists
-            ? Mono.error(new InvalidUserException("El email ya se encuentra registrado"))
-            : Mono.empty());
   }
 }
--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/UserPostgresReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/UserPostgresReactiveRepositoryAdapter.java
@@ -1,11 +1,13 @@
 package co.com.pragma.r2dbc;
 
 import co.com.pragma.model.user.User;
+import co.com.pragma.model.user.exceptions.InvalidUserException;
 import co.com.pragma.model.user.gateways.UserRepository;
 import co.com.pragma.r2dbc.entity.UserEntity;
 import co.com.pragma.r2dbc.helper.ReactiveAdapterOperations;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.reactive.TransactionalOperator;
 import reactor.core.publisher.Mono;
 
 @Repository
@@ -16,23 +18,25 @@ public class UserPostgresReactiveRepositoryAdapter extends ReactiveAdapterOperat
     UserPostgresReactiveRepository
     > implements UserRepository {
 
+  private final TransactionalOperator transactionalOperator;
+
   public UserPostgresReactiveRepositoryAdapter(UserPostgresReactiveRepository repository,
-      ObjectMapper mapper) {
+      ObjectMapper mapper, TransactionalOperator transactionalOperator) {
     /**
      *  Could be use mapper.mapBuilder if your domain model implement builder pattern
      *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
      *  Or using mapper.map with the class of the object model
      */
     super(repository, mapper, d -> mapper.map(d, User.class/* change for domain model */));
+    this.transactionalOperator = transactionalOperator;
   }
 
   @Override
   public Mono<User> register(User user) {
-    return super.save(user);
-  }
-  
-  @Override
-  public Mono<Boolean> existUserByEmail(String email) {
-    return repository.existsByEmail(email);
+    return repository.existsByEmail(user.getEmail())
+        .flatMap(exists -> exists
+            ? Mono.error(new InvalidUserException("El email ya se encuentra registrado"))
+            : super.save(user))
+        .as(transactionalOperator::transactional);
   }
 }
